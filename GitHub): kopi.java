682cc99a3685432c87475742d37259a4a5adcb91
Koldmunch <ChristofferMunch@Christoffers-MacBook-Pro.local>
2014-09-29 13:24:36 +0200

    Java

---
+++ GitHub): kopi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main (String args []) {
+	}
+	
+	Scanner user_input = new Scanner( System.in );
+	
+	String first_name;
+	first_name = user_input.next( );
+	
+
+}
